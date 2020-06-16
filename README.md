97821d74ccbb1d9a7acf91ef21ecca1ca73af7c5
Harmeher025 <66472848+Harmeher025@users.noreply.github.com>
2020-06-16 16:53:52 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Crumbled-Balls-3-
+# MatterJSBoilerPlate
+MatterJSBoilerPlate
---
+++ sketch.js
@@ -0,0 +1,81 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+const Constraint = Matter.Constraint;
+var dustbinObj, paperObject,groundObject
+var world, sling;
+var box1,box2,box3;
+
+
+function setup() {
+createCanvas(800, 700);
+rectMode(CENTER);
+
+
+engine = Engine.create();
+world = engine.world;
+
+paperObject=new paper(200,550,70);
+groundObject=new Ground(width/2,670,width,20);
+//dustbinObj=new Box(1200,500);
+
+//box1=new Box(520,620,20,100);
+// box2=new Box(630,660,200,20);
+//box3=new Box(740,620,20,100);
+
+
+box1=new Box(485,495,20,348);
+box2=new Box(630,660,270,20);
+box3=new Box(767,495,20,348);
+
+sling=new Slingshot(paperObject.body,{x:200,y:100})
+
+Engine.run(engine);
+
+ 
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(230);
+ 
+ 
+  paperObject.display();
+  groundObject.display();
+ // dustbinObj.display();
+  box1.display();
+  box2.display();
+  box3.display();
+  sling.display();
+
+ 
+ 
+ 
+ 
+ 
+ 
+}
+
+function keyPressed() {
+  if (keyCode === UP_ARROW) {
+
+    Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:60,y:-150});
+
+   
+  }
+}
+
+function mouseDragged() {
+   Matter.Body.setPosition(paperObject.body,{x:mouseX,y:mouseY})
+
+}
+
+function mouseReleased() {
+  sling.fly()
+}
+
+
+
